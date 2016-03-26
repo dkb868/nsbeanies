56e447a3e168f6c223ce6d315ec20b34f23e1df9
Dmitri Brereton <mitrikyle@yahoo.com>
2016-03-26 09:57:37 -0400

    too tired 2 care

--- nsbeanies.py
+++ nsbeanies.py
@@ -75,6 +75,68 @@ def get_stats(channelId):
 
     message = client.messages.create(to="+14804943646", from_="+12028499415",
                                          body="Your child may be a victim of cyberbullying, go to beanie's website for more information. ")
+
+
+    import smtplib
+    # Here are the email package modules we'll need
+    from email.mime.image import MIMEImage
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    userAddress ="postmaster@sandboxf32a8e5d26ea4769a1e20168754971a5.mailgun.org"
+    userPassword ="8e14d70dd060c158212a4f7b438f7b79"
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = 'Beanies Updates - Potential '
+    msg['From'] = userAddress
+    msg['To'] = 'krystal.folkes@gmail.com'
+    # Create the body of the message (a plain-text and an HTML version).
+
+    html = """\
+    <html>
+    <head>
+      <title>Beanies</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Beanies Notification</h1>
+        <p>Hello,<br>
+         <p> We would like to inform you that your child may be a potential victim of Cyberbullying. </p>
+           Login to  <a href="http://localhost:5000">Beanies</a> for more information
+        </p>
+      </div>
+      <p>Thanks.</p>
+    <br>
+    <br>
+    <br>
+    <p>Beanies</p>
+      <p>"Reduce the trend of Cyberbullying for YouTube Kids".</p>
+    </div>
+    </body>
+    </html>
+    """
+
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+
+    msg.attach(part2)
+
+    server = smtplib.SMTP('smtp.mailgun.org', 587)
+    server.ehlo()
+    server.starttls()
+
+    server.login(userAddress, userPassword)
+    server.sendmail(userAddress,"krystal.folkes@gmail.com", msg.as_string())
+    server.quit()
     return render_template('channel_stats.html', sentiment=sentiment, sentiments=sentiments)
 
 # route to post a channel
