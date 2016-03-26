a4f603295b84e2267201a22e988153d84de136af
Krystal Folkes <krystl.folkes@gmail.com>
2016-03-26 09:55:08 -0400

    add finished email

--- nsbeanies.py
+++ nsbeanies.py
@@ -28,26 +28,70 @@ import smtplib
 # Here are the email package modules we'll need
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-msg = MIMEMultipart()
+from email.mime.text import MIMEText
+
 userAddress ="postmaster@sandboxf32a8e5d26ea4769a1e20168754971a5.mailgun.org"
 userPassword ="8e14d70dd060c158212a4f7b438f7b79"
-server = smtplib.SMTP('smtp.mailgun.org', 587)
-server.starttls()
-server.login(userAddress, userPassword)
 
-msg = "YOUR MESSAGE!"
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'Beanies Updates - Potential '
+msg['From'] = userAddress
+msg['To'] = 'krystal.folkes@gmail.com'
+# Create the body of the message (a plain-text and an HTML version).
+
 html = """\
 <html>
-  <head></head>
-  <body>
-    <p>Hi!<br>
-       How are you?<br>
-       Here is the <a href="http://www.python.org">link</a> you wanted.
+
+
+<head>
+  <title>Beanies</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>Beanies Notification</h1>
+
+    <p>Hello,<br>
+     <p> We would like to inform you that your child may be a potential victim of Cyberbullying. </p>
+       Login to  <a href="http://localhost:5000">Beanies</a> for more information
     </p>
-  </body>
+  </div>
+  <p>Thanks.</p>
+<br>
+<br>
+<br>
+<p>Beanies</p>
+  <p>"Reduce the trend of Cyberbullying for YouTube Kids".</p>
+</div>
+
+</body>
+
+
+
+
 </html>
 """
-server.sendmail(userAddress,"krystal.folkes@gmail.com", msg)
+
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+
+msg.attach(part2)
+
+server = smtplib.SMTP('smtp.mailgun.org', 587)
+server.ehlo()
+server.starttls()
+
+server.login(userAddress, userPassword)
+server.sendmail(userAddress,"krystal.folkes@gmail.com", msg.as_string())
 server.quit()
 
 for comments in db.nsbeanie_comments.find():
